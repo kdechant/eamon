c45b92ea99cb66357f8bd21d50bd6b901ef0451b
Keith Dechant <keith.dechant@gmail.com>
2016-10-30 15:50:47 -0700

    Fix WSGI; add some packages/files needed for running on Apache server

--- eamon/wsgi.py
+++ eamon/wsgi.py
@@ -8,9 +8,25 @@ https://docs.djangoproject.com/en/1.9/howto/deployment/wsgi/
 """
 
 import os
+import time
+import traceback
+import signal
+import sys
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eamonproj.settings")
+sys.path.append('/var/www/vhosts/eamon')
+sys.path.append('/var/www/vhosts/eamon/venv/lib/python3.4/site-packages')
 
-application = get_wsgi_application()
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "eamon.settings")
+
+try:
+    application = get_wsgi_application()
+    print('WSGI without exception')
+except Exception:
+    print('handling WSGI exception')
+    # Error loading applications
+    if 'mod_wsgi' in sys.modules:
+        traceback.print_exc()
+        os.kill(os.getpid(), signal.SIGINT)
+        time.sleep(2.5)
