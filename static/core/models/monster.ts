ff932a1e225e7a7efc310c034ad4f06f707d67d7
Keith Dechant <keith.dechant@gmail.com>
2017-01-27 03:31:47 -0800

    Fix bug in spell success calculation
    It was decreasing your spell ability before the success roll, and using power ability incorrectly for other spells.

--- static/core/models/monster.ts
+++ static/core/models/monster.ts
@@ -882,19 +882,19 @@ export class Monster extends GameObject {
       return;
     }
 
-    // temporarily decrease spell ability
-    this.spell_abilities[spell_name] = Math.round(this.spell_abilities[spell_name] / 2);
+    let success: boolean = false;
 
     // roll to see if the spell succeeded
     let roll = game.diceRoll(1, 100);
     if (roll === 100) {
 
       game.history.write("The strain of attempting to cast " + spell_name.toUpperCase() + " overloads your brain and you forget it completely for the rest of this adventure.");
-      game.player.spell_abilities.power = 0;
+      game.player.spell_abilities[spell_name] = 0;
 
       // always a 5% chance to work and a 5% chance to fail
-    } else if (roll <= game.player.spell_abilities.power || roll <= 5 && roll <= 95) {
+    } else if (roll <= game.player.spell_abilities[spell_name] || roll <= 5 && roll <= 95) {
       // success!
+      success = true;
 
       // check for ability increase
       let inc_roll = game.diceRoll(1, 100);
@@ -903,10 +903,14 @@ export class Monster extends GameObject {
         game.history.write("Spell ability increased!", "success");
       }
 
-      return true;
     } else {
       game.history.write("Nothing happens.");
     }
+
+    // temporarily decrease spell ability
+    this.spell_abilities[spell_name] = Math.round(this.spell_abilities[spell_name] / 2);
+
+    return success;
   }
 
   /**
