d4b74a999eb129ad9b4973e46baf926e327aaad0
Keith Dechant <keith.dechant@gmail.com>
2017-02-05 14:14:17 -0800

    Added adventure: Escape from the Orc Lair
    This is a fairly dull adventure, but it doesn't require any custom code, so it was easy to port.

---
+++ static/adventures/escape-from-the-orc-lair/event-handlers.ts
@@ -0,0 +1,20 @@
+import {Game} from "../../core/models/game";
+import {Artifact} from "../../core/models/artifact";
+import {Monster} from "../../core/models/monster";
+import {RoomExit} from "../../core/models/room";
+import {Room} from "../../core/models/room";
+import {ReadCommand, OpenCommand} from "../../core/commands/core-commands";
+
+// NOTE: nothing special here. This adventure runs on the base code.
+
+export var event_handlers = {
+
+  // every adventure should have a "power" event handler.
+  // 'power' event handler takes a 1d100 dice roll as an argument.
+  // this event handler only runs if the spell was successful.
+  "power": function(roll) {
+    let game = Game.getInstance();
+    game.history.write("You hear a loud sonic boom which echoes all around you!");
+  },
+
+}; // end event handlers
