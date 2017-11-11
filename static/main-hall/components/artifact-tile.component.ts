50de8ad62c90e4b0200148dc5a840a8ca6d74282
Keith Dechant <keith.dechant@gmail.com>
2017-11-10 21:04:07 -0800

    More styling for shop and refactoring (2/2)

---
+++ static/main-hall/components/artifact-tile.component.ts
@@ -0,0 +1,62 @@
+import {Component, Input} from "@angular/core";
+import {
+  trigger,
+  state,
+  style,
+  animate,
+  transition
+} from '@angular/animations';
+
+import {PlayerService} from '../services/player.service';
+
+@Component({
+  selector: "artifact-tile",
+  templateUrl: "/static/main-hall/templates/artifact-tile.html",
+  animations: [
+    trigger('messageAnimation', [
+      state('visible', style({
+        opacity: 0.9,
+        display: 'block',
+      })),
+      state('hidden',   style({
+        opacity: 0,
+        display: 'none',
+      })),
+      transition('hidden => visible', animate('175ms ease-in')),
+      transition('visible => hidden', animate('175ms ease-out'))
+    ]),
+    trigger('sellAnimation', [
+      transition(':leave', animate(250, style({opacity: 0})))
+    ])
+  ]
+})
+export class ArtifactTileComponent {
+  @Input() artifact;
+  @Input() action;
+
+  constructor(private _playerService: PlayerService) {
+  }
+
+  buy(artifact) {
+    this._playerService.player.inventory.push(artifact);
+    this._playerService.player.gold -= artifact.value;
+    artifact.message = ("Bought!");
+    artifact.messageState = "visible";
+    setTimeout(function() { artifact.messageState = "hidden" }, 2000);
+  }
+
+  sell(artifact) {
+    artifact.salePending = true;
+    artifact.message = ("Sold!");
+    artifact.messageState = "visible";
+    var player = this._playerService.player;
+
+    setTimeout(function() {
+      let index = player.inventory.indexOf(artifact);
+      if (index > -1) {
+        player.inventory.splice(index, 1);
+      }
+      player.gold += Math.floor(artifact.value / 2);
+    }, 1250);
+  }
+}
