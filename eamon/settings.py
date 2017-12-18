476d9181e27b2ed90b260ce86f20a809609e851a
Keith Dechant <keith.dechant@gmail.com>
2017-12-18 01:27:46 -0800

    Turn off Django debug mode

--- eamon/settings.py
+++ eamon/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'd&q-j$g@zvm(@slk$wny&m=l%-%v)eiq3#z)mk1^%dj%aq&hi9'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
