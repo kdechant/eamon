dcaad8363506bb608f52731cea74d01e367b2160
Keith Dechant <keith.dechant@gmail.com>
2017-12-18 00:43:50 -0800

    Convert the command list into a modal

--- static/core/components/command-list.component.ts
+++ static/core/components/command-list.component.ts
@@ -1,26 +1,27 @@
 import {Component, Input} from "@angular/core";
+import {NgbModal} from "@ng-bootstrap/ng-bootstrap";
 
 @Component({
   selector: "command-list",
-  template: `
-    <button class="btn command-list-button" (click)="openCommands()">Commands</button>
-    <div class="command-list row" *ngIf="open">
-      <div class="command-list-item col-sm-3" *ngFor="let cmd of keys">
-        {{ cmd }}
-      </div>
-    </div>
-    `,
+  templateUrl: "/static/core/templates/command-list.html",
 })
 export class CommandListComponent {
   @Input() game;
-  open = false;
   keys: string[] = [];
 
-  public openCommands() {
+  constructor(private modalService: NgbModal) { }
+
+  public open(content) {
     if (this.keys.length === 0 && typeof this.game !== 'undefined') {
       this.keys = Object.keys(this.game.command_parser.available_verbs)
         .filter(c => c !== 'xgoto' && c !== 'xdebugger' && c !== 'xaccio');
     }
-    this.open = !this.open;
+
+    this.modalService.open(content).result.then((result) => {
+      console.log(`Closed with: ${result}`);
+    }, (reason) => {
+      console.log(`Dismissed`);
+    });
   }
+
 }
