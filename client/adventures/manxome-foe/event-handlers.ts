637f0a0c14f7c6f679de55bc4973175cff066fda
Keith Dechant <keith.dechant@gmail.com>
2019-11-30 21:55:35 -0800

    New adventure: The Manxome Foe

---
+++ client/adventures/manxome-foe/event-handlers.ts
@@ -0,0 +1,45 @@
+import Game from "../../core/models/game";
+import {Artifact} from "../../core/models/artifact";
+import {Monster} from "../../core/models/monster";
+import {RoomExit} from "../../core/models/room";
+import {Room} from "../../core/models/room";
+
+// The "game" object contains the event handlers and custom commands defined for the loaded adventure.
+declare var game;
+
+export var event_handlers = {
+
+  "start": function(arg: string) {
+    let game = Game.getInstance();
+  },
+
+  "seeMonster": function (monster: Monster): void {
+    // tweedledee/tweedledum share a description, as do the raths
+    if (monster.id === 4 || monster.id === 17) {
+      game.history.suppressNextMessage = true;
+    }
+  },
+
+  // every adventure should have a "power" event handler.
+  // 'power' event handler takes a 1d100 dice roll as an argument.
+  // this event handler only runs if the spell was successful.
+  "power": function(roll) {
+    let game = Game.getInstance();
+    if (roll <= 50) {
+      game.history.write("You hear a loud sonic boom which echoes all around you!");
+    } else if (roll <= 75) {
+      // teleport to random room
+      game.history.write("You are being teleported...");
+      let room = game.rooms.getRandom();
+      game.player.moveToRoom(room.id);
+      game.skip_battle_actions = true;
+    } else {
+      game.history.write("All your wounds are healed!");
+      game.player.heal(1000);
+    }
+  },
+
+}; // end event handlers
+
+
+// declare any functions used by event handlers and custom commands
