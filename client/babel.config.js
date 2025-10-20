30e5c4f2cce8fb625e3175d57ef7873492488429
Keith Dechant <keith.dechant@breaktech.com>
2025-10-20 01:34:38 -0700

    React 18 -> 19

--- client/babel.config.js
+++ client/babel.config.js
@@ -1,7 +1,12 @@
 module.exports = {
   presets: [
-    ['@babel/preset-env', {targets: {node: 'current'}}],
-    '@babel/preset-react',
-    '@babel/preset-typescript',
+    ["@babel/preset-env", { targets: { node: "current" } }],
+    [
+      "@babel/preset-react",
+      {
+        runtime: "automatic",
+      },
+    ],
+    "@babel/preset-typescript",
   ],
 };
